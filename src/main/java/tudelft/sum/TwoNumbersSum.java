8aef70476cacc8758db2405803b732caf06173e4
luciernax <edenmtbled@gmail.com>
2022-12-28 09:38:35 -0600

    Code modified

--- src/main/java/tudelft/sum/TwoNumbersSum.java
+++ src/main/java/tudelft/sum/TwoNumbersSum.java
@@ -10,22 +10,28 @@ class TwoNumbersSum {
         Collections.reverse(first);
         Collections.reverse(second);
 
+
         int complement = 0;
         ArrayList<Integer> result = new ArrayList<>();
 
         for(int i = 0; i < Math.max(first.size(), second.size()); i++){
             int firstVal = i < first.size() ? first.get(i) : 0;
             int secondVal = i < second.size() ? second.get(i) : 0;
-            int total = firstVal + secondVal + complement;
-            complement = 0;
+            int total = firstVal + secondVal + complement;  //[9,9,9,9,9,9,9]
+            complement = 0;                                 //      [9,9,9,9]
+                                                            // 0,0,0,9,9,9,8
             if (total >= 10){
                 complement = 1;
                 total -= 10;
             }
             result.add(i, total);
         }
+        if (complement==1)
+        {
+            result.add(complement);
+        }
 
-        Collections.reverse(result);
+        //Collections.reverse(result);
         return result;
     }
 }
